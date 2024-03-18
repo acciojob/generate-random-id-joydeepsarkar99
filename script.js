6feb1b3f0b8ef8efe93c8c2c7e702e6356f0303b
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-18 13:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function makeid(l) {
-  // write your code here
+let ans = ""
+	while(l != 0){
+	  let n = Math.floor(Math.random() * 3)
+	  if(n == 0){
+	    ans += String.fromCharCode((Math.floor(Math.random() * 26)) + 97) //lowercase
+	  }
+	  else if(n == 1){
+		  ans += Math.floor(Math.random() * 10) //digit
+	  }
+	  else if(n == 2){
+		  ans += String.fromCharCode((Math.floor(Math.random() * 26)) + 65) //uppercase
+	  }
+		l--
+	}
+	return ans
 }
 
 // Do not change the code below.
